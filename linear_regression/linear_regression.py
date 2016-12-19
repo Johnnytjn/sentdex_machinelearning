ff05baaa0ab44f318c51095cd8807771f261d042
carson <carsonjamescook@gmail.com>
2016-12-19 00:50:59 -0800

    Messed around with the graph a bit.

--- linear_regression/linear_regression.py
+++ linear_regression/linear_regression.py
@@ -40,14 +40,25 @@ def regression_line_and_graph(x_vals, y_vals, m, b):
     # average speed in mph
     avg_speed = m * 60
 
+    # Make prediction
+    predict_distance = 40
+    predict_time = m * predict_distance + b
+
     # Style, Title & Axis Labels
-    style.use("dark_background")
-    plt.suptitle("Avg. Speed: {num: .{dig}f} mph".format(num=avg_speed, dig=1))
+    suptitle_text = "Avg. Speed: {num: .{dig}f} mph".format(num=avg_speed,
+                                                            dig=1)
+    title_text = "Predicted{num: .{dig}f} min to "\
+                 "travel {dis} mi".format(num=predict_time, dig=1,
+                                          dis=predict_distance)
+    style.use("fivethirtyeight")
+    plt.suptitle(suptitle_text, ha="center", fontsize=14)
+    plt.title(title_text, ha="center", fontsize=10)
     plt.xlabel("Distance Driven (miles)")
     plt.ylabel("Time Taken (minutes)")
 
     # Plot
-    plt.scatter(x_vals, y_vals)
+    plt.scatter(x_vals, y_vals, s=100)
+    plt.scatter(predict_distance, predict_time, s=100, color='r')
     plt.plot(x_vals, regression_line)
 
     # Display
